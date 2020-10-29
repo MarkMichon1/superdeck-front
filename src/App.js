72215680959de0c1527c7ec958ac2805ea4c0b3f
Mark Michon <markmichon7@gmail.com>
2020-10-29 14:26:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthTest from "./Components/Administrative/AuthTest";
+import axiosInstance from "./Authentication/Api";
+import Login from "./Components/User/Login/Login"
+import Register from "./Components/User/Register/Register"
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  async handleLogout() {
+    try {
+      const response = await axiosInstance.post('users/token/blacklist/', {
+        "refresh_token": localStorage.getItem("refresh_token")
+      });
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      axiosInstance.defaults.headers['Authorization'] = null;
+      return response;
+    }
+    catch (e) {
+      console.log(e);
+    }
+  };
+
+
+  render() {
+    return (
+        <Fragment>
+          <nav>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/login/"}>Login</Link>
+            <Link to={"/register/"}>Register</Link>
+            <Link to={"/auth-test/"}>Auth Test</Link>
+            <button onClick={this.handleLogout}>Logout</button>
+          </nav>
+          <main>
+            <h1>PyroSource</h1>
+            <Switch>
+              <Route exact path={"/login/"} component={Login}/>
+              <Route exact path={"/register/"} component={Register}/>
+              <Route exact path={"/auth-test/"} component={AuthTest}/>
+              <Route path={"/"} render={() => <div>Home</div>}/>
+            </Switch>
+          </main>
+        </Fragment>
+    );
+  }
 }
 
 export default App;
